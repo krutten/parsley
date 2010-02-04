38d5f4dadd087f8bab6fd7218ce9b367daf20bde
krutten <krutten@domU-12-31-38-00-51-A7.compute-1.internal>
2010-02-03 18:05:09 -0800

    Build script

---
+++ build_install.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+   parsley_version="parsley 0.1"
+
+   # test for parsley
+   parsley_result=`/usr/local/bin/parsley -V`
+   if [[ $parsley_result != $parsley_version ]]; then
+
+   # /etc/config.yml
+    if ! test -e /usr/local/include/json/json.h; then
+      echo json not found, installing
+   # Build Install json-c 0.8
+      cd json-c-0.8
+      sh ./configure
+      make
+      make install
+      cd ..
+    else
+      echo json found, skipping
+    fi
+
+    # build parsley
+    echo installing parsley
+    sh ./configure
+    make
+    make install
+    ldconfig
+   else
+    echo already installed
+   fi
